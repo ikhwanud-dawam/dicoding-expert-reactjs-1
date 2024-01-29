fa948a8826f4bfb06b85cb0c935efe8c3ca54a07
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-01-29 23:05:04 +0700

    feat(components): Create LoginInput component
    - Added a new component for user login input fields.
    - Uses Chakra UI components for form elements.
    - Includes functionality for handling email and password input.
    - Implements a toggle for password visibility.
    - Provides a submit button for the login form.

---
+++ src/components/LoginInput.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import useInput from "../hooks/useInput";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+
+export default function LoginInput() {
+  const [email, onEmailChange] = useInput("");
+  const [password, onPasswordChange] = useInput("");
+
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickVisibility = () => setShowPassword(!showPassword);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl mt={4} id="email" isRequired>
+        <FormLabel>Email address</FormLabel>
+        <Input
+          type="email"
+          value={email}
+          onChange={onEmailChange}
+          placeholder="n@example.com"
+        />
+      </FormControl>
+      <FormControl mt={4} id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={onPasswordChange}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClickVisibility}>
+              {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button w="full" mt="6" colorScheme="teal" variant="solid" type="submit">
+        Sign In
+      </Button>
+    </form>
+  );
+}
