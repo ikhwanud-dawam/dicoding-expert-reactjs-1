e2291b13ae06ad976c6fd61596eeac6718ddcc5b
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-10 00:59:16 +0700

    feat: add new pages and components to routing structure
    - Integrate new pages and components into routing structure
    - Separate routing elements into individual files
    - Update routing paths and elements accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,25 @@ import {
 } from "react-router-dom";
 
 import Layout from "./layout/Layout";
-import RequireAuth from "./components/RequireAuth";
+import RequireAuth from "./layout/RequireAuth";
+import NotFound from "./components/NotFound";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Leaderboards from "./pages/Leaderboards";
+import AddThread from "./pages/AddThread";
+import DetailPage from "./pages/DetailPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<p>Tidak ditemukan</p>}>
-      <Route path="login" element={<p>Login</p>} />
-      <Route path="register" element={<p>Register</p>} />
+    <Route path="/" element={<Layout />} errorElement={<NotFound />}>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route element={<RequireAuth />}>
-        <Route index element={<p>Ini homepage threads</p>}/>
-        <Route path="threads/:id" element={<p>Ini thread</p>}/>
-        <Route path="leaderboards" element={<p>Leaderboards</p>}/>
+        <Route index element={<Homepage />} />
+        <Route path="thread/:id" element={<DetailPage />} />
+        <Route path="add" element={<AddThread />} />
+        <Route path="leaderboards" element={<Leaderboards />} />
       </Route>
     </Route>
   )
