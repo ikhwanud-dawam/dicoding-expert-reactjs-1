b349646a7517cd63c5e101d1484ab6f0946c1f1a
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-09 16:33:12 +0700

    refactor(states/leaderboards): integrate loading indicators into async actions

--- src/states/leaderboards/action.js
+++ src/states/leaderboards/action.js
@@ -1,4 +1,5 @@
 import api from "../../utils/api"
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
 
 const ActionType = {
   RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS',
@@ -15,12 +16,16 @@ function receiveLeaderboardsActionCreator(leaderboards) {
 
 function asyncReceiveLeaderboards() {
   return async (dispatch) => {
+    dispatch(showLoading())
+
     try {
       const leaderboards = await api.seeLeaderboards();
       dispatch(receiveLeaderboardsActionCreator(leaderboards));
     } catch (error) {
       alert(error.message);
     }
+    
+    dispatch(hideLoading())
   }
 }
 
