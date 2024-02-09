69340010700d0990ea7e40683d8b8db7c24853d3
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-10 01:49:03 +0700

    feat(states/users): Add user-related actions
    - Define action creators for receiving users and registering users
    - Implement asyncRegisterUser function to handle user registration process
    - Utilize try-catch block to handle potential errors during registration
    - Dispatch loading actions to indicate the loading state during the registration process

---
+++ src/states/users/action.js
@@ -0,0 +1,36 @@
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+function receiveUsersActionCreator(users){
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: {
+      users,
+    },
+  };
+}
+
+function asyncRegisterUser({ name, email, password, successCallback }) {
+  return async (dispatch) => {
+    dispatch(showLoading());
+
+    try {
+      await api.register({ name, email, password });
+      successCallback();
+    } catch (error) {
+      alert(error.message);
+    }
+    
+    dispatch(hideLoading());
+  };
+}
+
+export {
+  ActionType,
+  receiveUsersActionCreator,
+  asyncRegisterUser,
+};
