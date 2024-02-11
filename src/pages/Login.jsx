1c2c07ee46715f70408cfcb3684dd0187e55a524
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-11 21:40:02 +0700

    feat(pages): Integrate authentication functionality into Login page
    - Integrate authentication functionality into the Login page using Redux for state management.
    - Implement dispatch of asyncPreloadProcess action to handle preloading processes.
    - Utilize useEffect hook to dispatch asyncPreloadProcess action upon component mounting.
    - Implement onLogin function to dispatch asyncSetAuthUser action for user authentication.
    - Add conditional rendering to redirect authenticated users to the home page.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { Heading, Text, Link as ChakraLink, Flex, Box } from "@chakra-ui/react";
 import LoginInput from "../components/LoginInput";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncSetAuthUser } from "../states/authUser/action";
+import { useEffect } from "react";
+import { asyncPreloadProcess } from "../states/isPreload/action";
 
 function Login() {
+  const { authUser } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
+
+  const onLogin = ({ email, password }) => {
+    dispatch(asyncSetAuthUser({ email, password }));
+  };
+
+  if (authUser) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <Flex
       as="section"
@@ -24,7 +43,7 @@ function Login() {
         <Heading fontSize={"2xl"} mb={4} textAlign="center">
           Sign In
         </Heading>
-        <LoginInput />
+        <LoginInput login={onLogin} />
         <Text mt={4} textAlign="center">
           <ChakraLink as={Link} to={"/register"} color={"teal.500"}>
             Create new account?
