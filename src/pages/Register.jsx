125df0ce57f8767590649547df718221d388f7fb
Mohammad Ikhwanud Dawam <d4w4m123@gmail.com>
2024-02-11 21:41:51 +0700

    feat(pages): Add registration functionality to Register page
    - Implemented registration functionality in the Register page.
    - Utilized useEffect hook to dispatch asyncPreloadProcess action for preloading processes upon component mounting.
    - Implemented onRegister function to dispatch asyncRegisterUser action for user registration.
    - Added conditional rendering to redirect authenticated users to the home page.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Heading, Text, Link as ChakraLink, Flex, Box } from "@chakra-ui/react";
 import RegisterInput from "../components/RegisterInput";
+import { useEffect } from "react";
+import { asyncPreloadProcess } from "../states/isPreload/action";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncRegisterUser } from "../states/users/action";
 
 function Register() {
+  const { authUser } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
+
+  const onRegister = ({ name, email, password }) => {
+    const successCallback = () => {
+      navigate("/");
+    };
+    dispatch(asyncRegisterUser({ name, email, password, successCallback }));
+  };
+
+  if (authUser) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <Flex
       as="section"
@@ -24,7 +47,7 @@ function Register() {
         <Heading fontSize={"2xl"} mb={4} textAlign="center">
           Create an account
         </Heading>
-        <RegisterInput />
+        <RegisterInput register={onRegister} />
         <Text mt={4} textAlign="center">
           Already have an account?{" "}
           <ChakraLink as={Link} to={"/login"} color={"teal.500"}>
